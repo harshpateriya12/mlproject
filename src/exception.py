a8d2cf3a886f51d05342aff7a138915f235adba2
harshpateriya12 <harsh.pateriya1@gmail.com>
2025-06-13 19:09:30 +0530

    Hyper Tuning

--- src/exception.py
+++ src/exception.py
@@ -7,6 +7,8 @@ def error_message_detail(error,error_detail:sys):
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
     file_name,exc_tb.tb_lineno,str(error)
     )
+     
+    return error_message
 
 
 class CustomException(Exception):
@@ -17,9 +19,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message   
 
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by Zero Error")
-        raise CustomException(e,sys)
